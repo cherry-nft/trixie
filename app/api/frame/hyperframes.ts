2ab5dce9c372c7273867b9736dca35e8f9d1cab2
sherchaudhary <35322925+sherchaudhary@users.noreply.github.com>
2024-02-23 20:11:51 -0800

    444

--- app/api/frame/hyperframes.ts
+++ app/api/frame/hyperframes.ts
@@ -3,7 +3,10 @@ import { NEXT_PUBLIC_URL } from '../../config'; // Ensure this path is correct b
 
 interface HyperFrame {
   frame: string;
-  [key: number]: string | ((text: string) => string);
+  1: string | ((text: string) => string);
+  2?: string | ((text: string) => string);
+  3?: string | ((text: string) => string);
+  4?: string | ((text: string) => string);
 }
 
 // Define the checkForCorrectText function
@@ -12,7 +15,7 @@ function checkForCorrectText(room: string, text: string): boolean {
     return true;
   }
   return false;
-} 
+}
 
 
 // Define the getHyperFrame function
--- app/config.ts
+++ app/config.ts
@@ -1,1 +1,1 @@
-export const NEXT_PUBLIC_URL = 'https://pixie-game.vercel.app/';
+export const NEXT_PUBLIC_URL = 'https://trixie-navy.vercel.app/';
